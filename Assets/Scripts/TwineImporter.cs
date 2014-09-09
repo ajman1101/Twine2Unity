1b2cb293b3ac76e6cd8016d2b9d5ba58e0370cd1
Andrew AJ Mandula <ajman@csh.rit.edu>
2014-09-09 16:13:38 -0400

    First commit; added Readme, License, example twine file, and TwineImporter

---
+++ Assets/Scripts/TwineImporter.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class TwineImporter : MonoBehaviour {
+
+	// Use this for initialization
+	public bool listedAll;
+	public string[] file;
+	public List <string> twineInfo;
+	
+	void Start () 
+	{
+		listedAll = false;
+		string path = Application.dataPath + @"\TwineFiles\simple.txt";
+		string temp;
+		string [] file;
+
+        try 
+        {
+        	//create a stream reader
+        	//get the data in the text file
+        	//close the stream reader
+        	StreamReader sr = new StreamReader(path);
+        	temp = sr.ReadToEnd();
+        	sr.Close();
+
+        	//parse large string by lines into an array
+        	file = temp.Split("\n"[0]);
+        	foreach(string s in file)
+        	{
+        		twineInfo.Add(s);
+        	}
+        } 
+
+        catch (FileNotFoundException e) 
+        {
+            Debug.Log("The process failed: {0}"+ e.ToString());
+        }
+
+        if(listedAll == false)
+		{
+			for(int i = 0; i < twineInfo.Count; i++)
+			{
+				if(i == twineInfo.Count)
+				{
+					listedAll = true;
+				}
+
+				Debug.Log(twineInfo[i]);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		
+	}
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+#Twine2Unity
+
+###Twine2Unity imports and gives programmers an interface, both programatically and in Unity Editor to change Twine content.
+
+Twine2Unity is being developed for users of Twine to easily bring thier writing into Unity to create Visual Novels or easily add dialog trees in Unity.
+
+This project is in active development! Please comment and leave issues to help steer development of Twine2Unity into a useful software tool for you!
+
+Watch and Star the repo to be up to date!
