1891b2aefcc090ea56580c1642e5f943fc99f587
David Wilson <daw4475@g.rit.edu>
2014-09-22 18:56:06 -0400

    Reverted to previous version of this file, unintentionally overwrite

--- Assets/Scripts/TwineImporter.cs
+++ Assets/Scripts/TwineImporter.cs
@@ -3,60 +3,109 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 
-public class TwineImporter : MonoBehaviour {
+public class TwineImporter : MonoBehaviour
+{
 
-	// Use this for initialization
-	public bool listedAll;
-	public string[] file;
-	public List <string> twineInfo;
-	
-	void Start () 
-	{
-		listedAll = false;
-		string path = Application.dataPath + @"\TwineFiles\simple.txt";
-		string temp;
-		string [] file;
+    // Use this for initialization
+    public List<string> twineInfo;
 
-        try 
+    void Start()
+    {
+        string path = Application.dataPath + @"\TwineFiles\simple.txt";
+
+        twineInfo = ReadTwineData(path);
+
+        //ShowTwineData(twineInfo);
+
+        ParseTwineData(twineInfo);
+    }
+
+    List<string> ReadTwineData(string path)
+    {
+        string temp;
+        string[] file;
+
+        try
         {
-        	//create a stream reader
-        	//get the data in the text file
-        	//close the stream reader
-        	StreamReader sr = new StreamReader(path);
-        	temp = sr.ReadToEnd();
-        	sr.Close();
+            //create a stream reader
+            //get the data in the text file
+            //close the stream reader
+            StreamReader sr = new StreamReader(path);
+            temp = sr.ReadToEnd();
+            sr.Close();
 
-        	//parse large string by lines into an array
-        	file = temp.Split("\n"[0]);
-        	foreach(string s in file)
-        	{
-        		twineInfo.Add(s);
-        	}
-        } 
-
-        catch (FileNotFoundException e) 
-        {
-            Debug.Log("The process failed: {0}"+ e.ToString());
+            //parse large string by lines into an array
+            file = temp.Split("\n"[0]);
+            foreach (string s in file)
+            {
+                twineInfo.Add(s);
+            }
+            return twineInfo;
         }
 
-        if(listedAll == false)
-		{
-			for(int i = 0; i < twineInfo.Count; i++)
-			{
-				if(i == twineInfo.Count)
-				{
-					listedAll = true;
-				}
+        catch (FileNotFoundException e)
+        {
+            Debug.Log("The process failed: {0}" + e.ToString());
+            return null;
+        }
+    }
 
-				Debug.Log(twineInfo[i]);
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
+    void ShowTwineData(List<string> data)
+    {
+        bool listedAll = false;
 
-		
-	}
+        if (listedAll == false)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (i == data.Count)
+                {
+                    listedAll = true;
+                }
+
+                Debug.Log(data[i]);
+            }
+        }
+    }
+
+    void ParseTwineData(List<string> data)
+    {
+        for (int i = 0; i < data.Count; i++)
+        {
+            if (data[i].IndexOf("[[") != -1)
+            {
+                int startTitle = data[i].IndexOf("[[") + 2;
+                int endTitle = data[i].IndexOf("|");
+                string title = data[i].Substring(startTitle, endTitle - startTitle);
+                int startLink = data[i].IndexOf("|") + 1;
+                int endLink = data[i].IndexOf("]]");
+                string link = data[i].Substring(startLink, endLink - startLink);
+                Debug.Log("Title: " + title + "\n Link: " + link);
+                //Debug.Log("Link: "+data[i]);
+            }
+            if (data[i].Length == 0)
+            {
+                Debug.Log("Blank: " + data[i]);
+            }
+            if (data[i].IndexOf("::") != -1)
+            {
+                int startPassage = data[i].IndexOf("::") + 2;
+                string passage = data[i].Substring(startPassage);
+                Debug.Log("Start of Passage: " + passage);
+            }
+            else
+            {
+                string content = data[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+
 }
