31a78ae0aaeb51e40599e1641bb825cf60b47940
Andrew AJ Mandula <ajman@csh.rit.edu>
2014-09-24 10:36:55 -0400

    created Twine Node class to hold each node's info
    this class will be used in conjunction with another (yet to be made
    class) that will store all TwineNodes and the current node.

--- Assets/Scripts/TwineImporter.cs
+++ Assets/Scripts/TwineImporter.cs
@@ -9,6 +9,8 @@ public class TwineImporter : MonoBehaviour
     // Use this for initialization
     public List<string> twineInfo;
 
+    List<TwineNode> twineData = new List<TwineNode>();
+
     void Start()
     {
         string path = Application.dataPath + @"\TwineFiles\simple.txt";
@@ -34,7 +36,7 @@ public class TwineImporter : MonoBehaviour
             temp = sr.ReadToEnd();
             sr.Close();
 
-            //parse large string by lines into an array
+            //parse large string by lines into an list
             file = temp.Split("\n"[0]);
             foreach (string s in file)
             {
@@ -72,31 +74,33 @@ public class TwineImporter : MonoBehaviour
     {
         for (int i = 0; i < data.Count; i++)
         {
-            if (data[i].IndexOf("[[") != -1)
-            {
-                int startTitle = data[i].IndexOf("[[") + 2;
-                int endTitle = data[i].IndexOf("|");
-                string title = data[i].Substring(startTitle, endTitle - startTitle);
-                int startLink = data[i].IndexOf("|") + 1;
-                int endLink = data[i].IndexOf("]]");
-                string link = data[i].Substring(startLink, endLink - startLink);
-                Debug.Log("Title: " + title + "\n Link: " + link);
-                //Debug.Log("Link: "+data[i]);
-            }
-            if (data[i].Length == 0)
-            {
-                Debug.Log("Blank: " + data[i]);
-            }
-            if (data[i].IndexOf("::") != -1)
-            {
-                int startPassage = data[i].IndexOf("::") + 2;
-                string passage = data[i].Substring(startPassage);
-                Debug.Log("Start of Passage: " + passage);
-            }
-            else
-            {
-                string content = data[i];
-            }
+        	TwineNode twineNode = new TwineNode();
+        	twineData.Add(twineNode.Parse(data[i]));
+            // if (data[i].IndexOf("[[") != -1)
+            // {
+            //     int startTitle = data[i].IndexOf("[[") + 2;
+            //     int endTitle = data[i].IndexOf("|");
+            //     string title = data[i].Substring(startTitle, endTitle - startTitle);
+            //     int startLink = data[i].IndexOf("|") + 1;
+            //     int endLink = data[i].IndexOf("]]");
+            //     string link = data[i].Substring(startLink, endLink - startLink);
+            //     Debug.Log("Title: " + title + "\n Link: " + link);
+            //     Debug.Log("Link: "+data[i]);
+            // }
+            // if (data[i].Length == 0)
+            // {
+            //     Debug.Log("Blank: " + data[i]);
+            // }
+            // if (data[i].IndexOf("::") != -1)
+            // {
+            //     int startPassage = data[i].IndexOf("::") + 2;
+            //     string passage = data[i].Substring(startPassage);
+            //     Debug.Log("Start of Passage: " + passage);
+            // }
+            // else
+            // {
+            //     string content = data[i];
+            // }
         }
     }
 
---
+++ Assets/Scripts/TwineNode.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TwineNode{
+
+	string title;
+	string passage; 
+	string link;
+	string content;
+
+
+	public string Title { get{return title;} set{title = value;}}
+	public string Passage {get{return passage;} set{passage = value;}}
+	public string Link {get{return link;} set{link = value;}}
+	public string Content {get{return content;} set{content = value;}}
+
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public TwineNode Parse(string data)
+	{
+		if (data.IndexOf("[[") != -1)
+            {
+                int startTitle = data.IndexOf("[[") + 2;
+                int endTitle = data.IndexOf("|");
+                title = data.Substring(startTitle, endTitle - startTitle);
+                int startLink = data.IndexOf("|") + 1;
+                int endLink = data.IndexOf("]]");
+                link = data.Substring(startLink, endLink - startLink);
+                Debug.Log("Title: " + title + "\n Link: " + link);
+                Debug.Log("Link: "+data);
+            }
+            if (data.Length == 0)
+            {
+                Debug.Log("Blank: " + data);
+            }
+            if (data.IndexOf("::") != -1)
+            {
+                int startPassage = data.IndexOf("::") + 2;
+                passage = data.Substring(startPassage);
+                Debug.Log("Start of Passage: " + passage);
+            }
+            else
+            {
+                content = data;
+            }
+            return  this;
+	}
+}
